443fd88220a30fe839e8cecb0d2faa2561f51047
Bruno Maurino <brunomaurino@Brunos-MacBook-Air.local>
2022-08-27 13:39:20 -0300

    Adding pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Detail from "./pages/detail/Detail";
+import New from "./pages/new/New";
+
 function App() {
   return (
     <div className="App">
-      Admin Panel
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Detail />} />
+              <Route path="new" element={<New />} />
+            </Route>
+
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Detail />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
